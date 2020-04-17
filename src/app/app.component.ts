1bb7076b16161f6e3a1de37f94bdc87c01a33769
LaunchCode Student <aspafford148@gmail.com>
2020-04-16 19:45:29 -0500

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment6';
+  title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+  
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[]=[];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+       this.displayList = matchingSatellites;
+  }
+
+  constructor(){
+    this.displayList = []
+    this.sourceList = []
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+        
+        let fetchedSatellites = data.satellites;
+          for(let i=0;i<fetchedSatellites.length;i++){
+            let satData=new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satData);
+          }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+    }.bind(this));
+   }
+  
 }
